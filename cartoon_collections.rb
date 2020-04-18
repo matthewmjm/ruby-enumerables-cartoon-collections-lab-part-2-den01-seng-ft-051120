e6494a1ee341b0d32f7f9d78027cdae96bc01b0f
Matthew Malecki <mmalecki4@gmail.com>
2020-04-18 16:51:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,27 +26,27 @@ def square_array(array)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |item1|
+
+
+###could not get capitalize to work no matter which way I wrote it 
+#def summon_captain_planet(planeteer_calls)
+#  planeteer_calls.map do |item1|
 #   item1.concat("!")
 #   item1.capitalize
-   item1.concat("!").capitalize
-   item1.capitalize.concat("!")
-  end
-  planeteer_calls
-end
-
-#def summon_captain_planet(planeteer_calls)
-#  newArray = []
-#  i = 0
-#  while i < planeteer_calls.length do
-#   newArray.push(planeteer_calls[i].concat("!").capitalize)  
-#  i += 1
+#   item1.concat("!").capitalize
+#   item1.capitalize.concat("!")
 #  end
-#  newArray
+#  planeteer_calls
 #end
-
-
+def summon_captain_planet(planeteer_calls)
+  newArray = []
+  i = 0
+  while i < planeteer_calls.length do
+   newArray.push(planeteer_calls[i].concat("!").capitalize)  
+  i += 1
+  end
+  newArray
+end
 
 
 
