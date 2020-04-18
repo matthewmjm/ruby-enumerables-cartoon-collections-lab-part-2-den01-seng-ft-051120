1938016238183bd982c0592d31c0a42ea1b75ada
Matthew Malecki <mmalecki4@gmail.com>
2020-04-18 16:48:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |item1|
-   item1.concat("!")
-   item1.capitalize
+#   item1.concat("!")
+ #  item1.capitalize
    item1.concat("!").capitalize
   end
   planeteer_calls
