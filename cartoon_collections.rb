02cb9064cadaf828eee159686ea6b65a2cb1cea2
Matthew Malecki <mmalecki4@gmail.com>
2020-04-18 16:52:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -56,9 +56,10 @@ def long_planeteer_calls(planeteer_calls)
   end
 end
 
+
+
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.detect
     planeteer_calls.include? valid_calls
-
 end
